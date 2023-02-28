64c9d21276f9473f23bfbeaf874d60b4681cc4c3
shivam.panicker@gmail.com <shivam.panicker@gmail.com>
2023-02-28 04:03:36 +0000

    dbutils notebook

--- src/main/tests/test_all.py
+++ src/main/tests/test_all.py
@@ -1,10 +1,14 @@
 # Databricks notebook source
-# MAGIC %run ./bronze/test_load_data_into_bronze
+abs_path = "/Repos/shivam.panicker@databricks.com/cicd_with_databricks/src/main/tests"
 
 # COMMAND ----------
 
-# MAGIC %run ./silver/test_transform_to_scd2
+dbutils.notebook.run(abs_path+"/bronze/test_load_data_into_bronze", 300, {})
 
 # COMMAND ----------
 
-# MAGIC %run ./silver/standardise_retail_dataset
+dbutils.notebook.run(abs_path+"/silver/test_transform_to_scd2", 300, {})
+
+# COMMAND ----------
+
+dbutils.notebook.run(abs_path+"/silver/standardise_retail_dataset", 300, {})
