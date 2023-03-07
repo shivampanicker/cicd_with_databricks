767b12c7f75b30de7f7b6ddecf530066e5593121
shivam.panicker@gmail.com <shivam.panicker@gmail.com>
2023-03-07 01:52:01 +0000

    test28

--- src/main/tests/integration_suite/integration_tests.py
+++ src/main/tests/integration_suite/integration_tests.py
@@ -8,14 +8,8 @@ import pytest
 # COMMAND ----------
 
 username = dbutils.notebook.entry_point.getDbutils(
-).notebook().getContext().userName().get().replace('.', '_')
-user = username[:username.index("@")]
-
-# COMMAND ----------
-
-bronze_path = f'/FileStore/{username}_bronze_db_test/bronze_customers'
-silver_path = f'/FileStore/{username}_silver_db_test/silver_customers'
-gold_path = f'/FileStore/{username}_silver_db_test/gold_customers'
+).notebook().getContext().userName().get()
+user = username[:username.index("@")].replace('.', '_')
 
 # COMMAND ----------
 
@@ -25,44 +19,44 @@ abs_path = f'/Repos/{username}/cicd_with_databricks/src/main/tests/'
 
 # COMMAND ----------
 
-#dbutils.notebook.run(abs_path + "cleanup_tests", 300, {})
+dbutils.notebook.run(abs_path + "cleanup_tests", 300, {})
 
 # COMMAND ----------
 
-# MAGIC %run ../cleanup_tests
+#%run ../src/main/tests/cleanup_tests
 
 # COMMAND ----------
 
-# MAGIC %run ../../python/setup/initiate_setup
+#%run ..src/main/python/setup/initiate_setup
 
 # COMMAND ----------
 
-#dbutils.notebook.run(abs_path + "../python/setup/initiate_setup", 300, {})
+dbutils.notebook.run(abs_path + "../python/setup/initiate_setup", 300, {})
 
 # COMMAND ----------
 
-# MAGIC %run ../../python/bronze/load_data_into_bronze
+#%run ..src/main/python/bronze/load_data_into_bronze
 
 # COMMAND ----------
 
-#dbutils.notebook.run(abs_path + "../python/bronze/load_data_into_bronze", 300, {})
+dbutils.notebook.run(abs_path + "../python/bronze/load_data_into_bronze", 300, {})
 
 # COMMAND ----------
 
-# MAGIC %run ../../python/silver/transform_to_scd2
+#%run ..src/main/python/silver/transform_to_scd2
 
 # COMMAND ----------
 
-#dbutils.notebook.run(abs_path + "../src/main/python/silver/transform_to_scd2
+dbutils.notebook.run(abs_path + "../python/silver/transform_to_scd2
 
 # COMMAND ----------
 
 # Test the bronze layer
 
 # Load the data into the bronze layer
-load_data_to_bronze(source_dataset, target_path)
+load_data_to_bronze(source_dataset, target_path.replace('.', '_'))
 
-bronze_df = spark.read.format("delta").load(bronze_path+"bronze_customers")
+bronze_df = spark.read.table(f"{user}_bronze_db_test.bronze_customers")
 assert bronze_df.count() == 10000  # Assumes that there are 10000 rows in the original data source
 
 # COMMAND ----------
@@ -74,7 +68,7 @@ spark.sql(f"drop table if exists {user}_silver_db_test.silver_customers")
 # Call the function to transform the customer data into a dimension table with SCD type 2
 transform_to_scd2(customer_df, mode="test")
 
-silver_df = spark.read.format("delta").load(silver_path)
+silver_df = spark.read.table(f"{user}_silver_db_test.silver_customers")
 
 assert silver_df.count() == 10000  # Assumes that there are 10000 rows in the original data source
 assert silver_df.columns == ["customer_id", "customer_name", "state", "company", "phone_numer", "start_date", "end_date"]
@@ -93,4 +87,4 @@ print("All integration tests passed!")
 
 # COMMAND ----------
 
-dbutils.notebook.run(abs_path+"/../src/main/tests/cleanup_tests", 300, {})
+dbutils.notebook.run(abs_path + "cleanup_tests", 300, {})
