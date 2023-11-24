e6fefff8b05ee8974fcc0bf6ebca14a849b16f66
arunvkhare@gmail.com <arunvkhare@gmail.com>
2023-11-24 17:44:33 +0000

    added assertion to silver unit test

--- src/main/tests/silver/test_standardise_retail_dataset.py
+++ src/main/tests/silver/test_standardise_retail_dataset.py
@@ -69,7 +69,7 @@ def test_transform_to_silver_2():
     assert expected_df.select("sale_date").collect() == actual_df.select("sale_date").collect()
     assert expected_df.select("currency").collect() == actual_df.select("currency").collect()
     assert expected_df.select("sale_id").collect() == actual_df.select("sale_id").collect()
-    
+    assert expected_df.select("sale_amount").collect() == actual_df.select("sale_amount").collect()
 
 # COMMAND ----------
 
